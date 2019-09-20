d06a96f6592e1a9cf18af698eb03d1382f90320f
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-09-19 21:41:32 -0400

    adds buttons (,C,CE,)

--- src/components/KeyPad.js
+++ src/components/KeyPad.js
@@ -8,6 +8,10 @@ export default class KeyPad extends Component {
     render() {
         return (
             <div className="buttons">
+                <button name="(" onClick={this.buttonPressed}>(</button>
+                <button name="C" onClick={this.buttonPressed}>C</button>
+                <button name="CE" onClick={this.buttonPressed}>CE</button>
+                <button name=")" onClick={this.buttonPressed}>)</button>
                 <button name="1" onClick={this.buttonPressed}>1</button>
                 <button name="2" onClick={this.buttonPressed}>2</button>
                 <button name="3" onClick={this.buttonPressed}>3</button>
