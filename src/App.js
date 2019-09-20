f55be7762bd670bbd010b9336e1d4377c4bb3d2c
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-09-19 20:25:36 -0400

    adds Output Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import KeyPad from "./components/KeyPad";
+import Output from "./components/Output";
 
 class App extends Component {
     render() {
         return (
-        <KeyPad />
+            <div>
+                <Output result="Calculator"/>
+                <KeyPad />
+            </div>
         )
     }
 }
