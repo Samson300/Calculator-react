3d58668200aea89cd420ca368aaa28fedb64d222
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-09-19 21:06:08 -0400

    adds css to center calculator on page

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
 
     render() {
         return (
-            <div>
+            <div className="App">
                 <Output result={this.state.result}/>
                 <KeyPad buttonPressed={this.buttonPressed} />
             </div>
