1275edee2ea202cc364b3e9bb47164f444c7a6c6
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-09-19 20:18:30 -0400

    adds KeyPad component with the number one button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import KeyPad from "./components/KeyPad";
 
 class App extends Component {
     render() {
-        return <div className="app">
-            <h1>Check check</h1>
-        </div>
+        return (
+        <KeyPad />
+        )
     }
 }
 
---
+++ src/components/KeyPad.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+export default class KeyPad extends Component {
+    render() {
+        return (
+            <div>
+                <button name="1">1</button>
+            </div>
+        )
+    }
+}
