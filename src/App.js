aa39657eecd2e7237dd50cd1b12fc5178a482f1c
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-09-19 20:53:56 -0400

    adds buttonPressed method so that when a button is clicked the state result is set to button name and is rendered to screen

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import KeyPad from "./components/KeyPad";
 import Output from "./components/Output";
 
 class App extends Component {
+
+    state = {
+        result: ""
+    }
+
+    buttonPressed = (buttonName) => {
+        this.setState({
+            result: buttonName
+        });
+    }
+
     render() {
         return (
             <div>
-                <Output result="Calculator"/>
-                <KeyPad />
+                <Output result={this.state.result}/>
+                <KeyPad buttonPressed={this.buttonPressed} />
             </div>
         )
     }
