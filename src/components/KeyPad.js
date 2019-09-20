7236b157fd5bca6813fd403fbfbf8ac128c173b5
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-09-19 20:20:59 -0400

    adds buttons 2,3,4,5,6,7,8,9,0,+,- to KeyPad component

--- src/components/KeyPad.js
+++ src/components/KeyPad.js
@@ -5,6 +5,17 @@ export default class KeyPad extends Component {
         return (
             <div>
                 <button name="1">1</button>
+                <button name="2">2</button>
+                <button name="3">3</button>
+                <button name="4">4</button>
+                <button name="5">5</button>
+                <button name="6">6</button>
+                <button name="7">7</button>
+                <button name="8">8</button>
+                <button name="9">9</button>
+                <button name="0">0</button>
+                <button name="+">+</button>
+                <button name="-">-</button>
             </div>
         )
     }
